c3a8e826b59d2c0254e12bdb1f54b307fddbe3f5
opsilonn <huguesbegeot@gmail.com>
2024-05-18 17:40:03 +0200

    Dédoubles la méthode getItem
    - dédouble la méthode en getItem et getFolder
    - renomme audioFolderPath en AUDIO_FOLDER_PATH

--- server/models/audio.model.ts
+++ server/models/audio.model.ts
@@ -4,7 +4,7 @@ import validator from "validator";
 import AudioItem from "../../models/models/audio-item";
 import PlaylistItemFilled from "../../models/models/playlist-item-filled";
 
-const audioFolderPath = "./static/audio";
+const AUDIO_FOLDER_PATH = "./static/audio";
 
 export default class AudioCRUD {
 
@@ -13,9 +13,11 @@ export default class AudioCRUD {
   /**
    * @returns {Promise<[]>}
    */
-  static async getAll(): Promise<{ audioFolder: PlaylistItemFilled[], audiosDatabase: AudioItem[] }> {
+  public static async getAll(): Promise<{ audioFolder: PlaylistItemFilled[], audiosDatabase: AudioItem[] }> {
     this.audiosDatabase = [];
-    const audioFolder: PlaylistItemFilled[] = await this.readFolder(audioFolderPath);
+    
+    const audioFolder: PlaylistItemFilled[] = await this.readFolder(AUDIO_FOLDER_PATH);
+
     return {
       audioFolder: audioFolder,
       audiosDatabase: this.audiosDatabase
@@ -27,7 +29,7 @@ export default class AudioCRUD {
    * @param {*} path Path of the current folder
    * @returns An object containing all its content
    */
-  static readFolder(path: string): PlaylistItemFilled[] {
+  private static readFolder(path: string): PlaylistItemFilled[] {
     const folder: PlaylistItemFilled[] = [];
 
     try {
@@ -35,10 +37,14 @@ export default class AudioCRUD {
       const filesName = fs.readdirSync(path);
 
       // We first add the folders
-      filesName.filter((fileName) => !fileName.includes(".")).forEach((fileName) => folder.push(this.GetItem(fileName, path, true)));
+      filesName
+        .filter((fileName) => !fileName.includes("."))
+        .forEach((fileName) => folder.push(this.getFolder(fileName, path)));
 
       // We then add the files
-      filesName.filter((fileName) => fileName.includes(".")).forEach((fileName) => folder.push(this.GetItem(fileName, path, false)));
+      filesName
+        .filter((fileName) => fileName.includes("."))
+        .forEach((fileName) => folder.push(this.getItem(fileName, path)));
     } catch (err: any) {
       console.log(err);
     }
@@ -50,15 +56,14 @@ export default class AudioCRUD {
    *
    * @param {string} fileName
    * @param {string} path
-   * @param {boolean} isFolder
    * @returns
    */
-  static GetItem(fileName: string, path: string, isFolder: boolean): PlaylistItemFilled {
+  private static getItem(fileName: string, path: string): PlaylistItemFilled {
     // On déclare nos variables
     let id: string;
     let name: string;
     let fullPath: string;
-    const fileId = fileName.substring(fileName.lastIndexOf(" ") + 1, isFolder ? fileName.length : fileName.lastIndexOf("."));
+    const fileId = fileName.substring(fileName.lastIndexOf(" ") + 1, fileName.lastIndexOf("."));
 
     if (validator.isUUID(fileId)) {
       id = fileId;
@@ -66,19 +71,46 @@ export default class AudioCRUD {
       fullPath = `${path}/${fileName}`;
     } else {
       id = uuidv4();
-      name = isFolder ? fileName : fileName.substring(0, fileName.lastIndexOf("."));
-      fullPath = `${path.replace('./', '')}/${name} ${id}` + (isFolder ? "" : fileName.substring(fileName.lastIndexOf("."), fileName.length));
+      name = fileName.substring(0, fileName.lastIndexOf("."));
+      fullPath = `${path.replace('./', '')}/${name} ${id}` + fileName.substring(fileName.lastIndexOf("."), fileName.length);
       fs.renameSync(`${path}/${fileName}`, fullPath);
     }
 
     // On construit l'audio
     const item: PlaylistItemFilled = new PlaylistItemFilled(null, id, "", [], name, fullPath.replace("./static", ""));
 
-    if (isFolder) {
-      item.children = this.readFolder(fullPath);
+    // On ajoute chaque musique à la BDD
+    this.audiosDatabase.push(new AudioItem(id, name, fullPath.replace("./static", "")));
+
+    return item;
+  }
+
+  /**
+   *
+   * @param {string} fileName
+   * @param {string} path
+   * @returns
+   */
+  private static getFolder(fileName: string, path: string): PlaylistItemFilled {
+    // On déclare nos variables
+    let id: string;
+    let name: string;
+    let fullPath: string;
+    const fileId = fileName.substring(fileName.lastIndexOf(" ") + 1, fileName.length);
+
+    if (validator.isUUID(fileId)) {
+      id = fileId;
+      name = fileName.substring(0, fileName.lastIndexOf(" "));
+      fullPath = `${path}/${fileName}`;
     } else {
-      this.audiosDatabase.push(new AudioItem(id, name, fullPath.replace("./static", "")));
+      id = uuidv4();
+      name = fileName;
+      fullPath = `${path.replace('./', '')}/${name} ${id}`;
+      fs.renameSync(`${path}/${fileName}`, fullPath);
     }
+
+    // On construit le dossier
+    const item: PlaylistItemFilled = new PlaylistItemFilled(null, id, "", this.readFolder(fullPath), name, fullPath.replace("./static", ""));
 
     return item;
   }
